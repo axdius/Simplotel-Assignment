acd12dcd303e9b35f86d1fdf8e28f03df39291fb
Prashant Kumar <124628326+axdius@users.noreply.github.com>
2024-01-23 19:13:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function navigateToSecondPage() {
+    let firstPage = document.getElementById('firstPage');
+    let secondPage = document.getElementById('secondPage');
+
+    firstPage.style.display = 'none';
+    secondPage.style.display = 'flex';
+}
